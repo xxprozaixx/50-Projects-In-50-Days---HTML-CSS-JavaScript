c5d55d1daa645d04afdf57bf9636f1b251748ded
Adrian Ng <93761558+xxprozaixx@users.noreply.github.com>
2023-04-11 14:57:37 +0700

    Incrementing Counter - Project 15

---
+++ incrementing-counter-15/script.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-counter');
+        const c = +counter.innerText;
+        console.log(target);
+
+        const increment = target / 200;
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
